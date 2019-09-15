a92b2d53f23e67bb79e39bc737f4511366507baf
leexiaoboo <leexiaoboo@outlook.com>
2019-09-15 14:51:04 +0800

    优化抽象工厂模式

--- src/main/java/com/lxb/myproduct/create/factory/abstractfactory/AppleFactory.java
+++ src/main/java/com/lxb/myproduct/create/factory/abstractfactory/AppleFactory.java
@@ -1,5 +1,8 @@
 package com.lxb.myproduct.create.factory.abstractfactory;
 
+/**
+ * 工厂生产的对象都是实现某一个接口的，面向接口编程。
+ */
 public class AppleFactory extends AbstarctFactory{
     @Override
     public Fuite getFuite() {
